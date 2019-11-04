75daa24e8e2b5c31d907d1e4f71dc0947bbb7cb6
Paulo Vilarinho <paulotarsobranco@hotmail.com>
2019-11-04 01:01:01 -0300

    chore: react navigation instalation and configuration

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
-
 import './config/ReactotronConfig';
-// import { Container } from './styles';
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome tll React Native</Text>
-    </View>
-  );
+  return <Routes />;
 }
+
+
